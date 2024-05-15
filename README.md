ecf10880cfda57d3d375470935fc619954e4a284
mollypie <54678969+mollypie@users.noreply.github.com>
2024-05-15 18:51:53 +0200

    add unit tests

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # qa_python
+
+```
+1. test_add_new_book_added_new_book() - проверяет добавление книги в коллекцию
+```
+
+```
+2. test_add_new_book_book_without_genre_returned_empty_string() - проверяет, что у добавленой книги нет жанра
+```
+
+```
+3. test_set_book_genre_existing_book_genre_successfully_applied() - проверяет добавление жанра книге
+```
+
+```
+4. test_get_book_genre_existing_book_returned_genre() - проверяет получение жанра книги по её названию
+```
+
+```
+5. test_get_books_with_specific_genre_one_genre_returned_list_books() - проверяет вывод в список книг с определённым жанром
+```
+
+```
+6. test_get_books_genre_returned_dictionary_with_books_and_genre() - проверяет получение данных словаря books_genre
+```
+
+```
+7. test_get_books_for_children_genre_without_rating_returned_one_book() - проверяет получение списка книг, подходящих детям (без рейтинга)
+```
+
+```
+8. test_add_book_in_favorites_one_book_added_to_list_favorites() - проверяет добавление книги в избранное
+```
+
+```
+9. test_delete_book_from_favorites_one_book_got_empty_list_favorites() - проверяет удаление книги из избранных
+```
+
+```
+10. test_get_list_of_favorites_books_two_books_returned_list_favorites_books() - проверяет получение списка избранных книг
+```
--- tests.py
+++ tests.py
@@ -1,24 +1,81 @@
+import pytest
+
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
+
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
+    def test_add_new_book_added_new_book(self):
+        book = BooksCollector()
+        book.add_new_book('Дюна')
 
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+        assert "Дюна" in book.books_genre
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+    def test_add_new_book_book_without_genre_returned_empty_string(self):
+        book = BooksCollector()
+        book.add_new_book('Дюна')
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+        assert book.get_book_genre('Дюна') == ''
+
+    def test_set_book_genre_existing_book_genre_successfully_applied(self):
+        book = BooksCollector()
+        book.books_genre = {'Дюна': ''}
+
+        book.set_book_genre('Дюна', 'Фантастика')
+
+        assert book.books_genre['Дюна'] == 'Фантастика'
+
+    def test_get_book_genre_existing_book_returned_genre(self):
+        book = BooksCollector()
+        book.books_genre = {'Дюна': 'Фантастика'}
+
+        genre = book.get_book_genre('Дюна')
+
+        assert genre == 'Фантастика'
+
+    def test_get_books_with_specific_genre_one_genre_returned_list_books(self):
+        book = BooksCollector()
+        book.books_genre = {'Дюна': 'Фантастика', 'Русалочка': 'Мультфильмы', 'Пикник на обочине': 'Фантастика'}
+
+        list_books = book.get_books_with_specific_genre('Фантастика')
+
+        assert list_books == ['Дюна', 'Пикник на обочине']
+
+    def test_get_books_genre_returned_dictionary_with_books_and_genre(self):
+        book = BooksCollector()
+        book.books_genre = {'Дюна': 'Фантастика'}
+
+        assert book.get_books_genre() == {'Дюна': 'Фантастика'}
+
+    @pytest.mark.parametrize('genre', ['Фантастика', 'Мультфильмы', 'Комедии'])
+    def test_get_books_for_children_genre_without_rating_returned_one_book(self, genre):
+        book = BooksCollector()
+        book.books_genre = {'Детская книжка': genre}
+
+        approved_book = book.get_books_for_children()
+
+        assert len(approved_book) == 1
+
+    def test_add_book_in_favorites_one_book_added_to_list_favorites(self):
+        book = BooksCollector()
+        book.books_genre = {'Дюна': 'Фантастика'}
+
+        book.add_book_in_favorites('Дюна')
+
+        assert len(book.favorites) == 1
+
+    def test_delete_book_from_favorites_one_book_got_empty_list_favorites(self):
+        book = BooksCollector()
+        book.favorites = ['Дюна']
+
+        book.delete_book_from_favorites('Дюна')
+
+        assert len(book.favorites) == 0
+
+    def test_get_list_of_favorites_books_two_books_returned_list_favorites_books(self):
+        book = BooksCollector()
+        book.favorites = ['Дюна', 'Пикник на обочине']
+
+        list_favorites_book = book.get_list_of_favorites_books()
+
+        assert list_favorites_book == ['Дюна', 'Пикник на обочине']
